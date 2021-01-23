f87c9cc764631856091dd8410cf7694328c637bc
Michael-4 <17maaa1@queensu.ca>
2021-01-23 04:12:21 -0500

    Merged backend waiting to merge with frontend

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
-from PyDictionary import PyDictionary
+import PyDictionary
 from nltk.corpus import stopwords  
 from nltk.tokenize import word_tokenize 
+from nltk import pos_tag
+import language_tool_python
 import string
+import get_wikipedia_links as app
 
 def mergeDictionary(d1, d2):
   d = {}
@@ -38,66 +41,125 @@ def countWords(words):
 
   return dictCount
       
-def replaceCharacter(s, newstring, index, nofail=False):
-    # raise an error if index is outside of the string
-    if not nofail and index not in range(len(s)):
-        raise ValueError("index outside given string")
+# def replaceCharacter(s, newstring, index, nofail=False):
+#     # raise an error if index is outside of the string
+#     if not nofail and index not in range(len(s)):
+#         raise ValueError("index outside given string")
 
-    # if not erroring, but the index is still not in the correct range..
-    if index < 0:  # add it to the beginning
-        return newstring + s
-    if index > len(s):  # add it to the end
-        return s + newstring
+#     # if not erroring, but the index is still not in the correct range..
+#     if index < 0:  # add it to the beginning
+#         return newstring + s
+#     if index > len(s):  # add it to the end
+#         return s + newstring
 
-    # insert the new string between "slices" of the original
-    return s[:index] + newstring + s[index + 1:]
+#     # insert the new string between "slices" of the original
+#     return s[:index] + newstring + s[index + 1:]
 
-def checkPunctuation(words):
-    #search each character for puntuation using a for loop in range length of string
-    for i in range(len(words)):
-      quoteCount = 0
-          #check if its a punctuation character
-      if words[i] == '"':
-        quoteCount += 1
-        break
-      elif(words[i] in string.punctuation):
-            if words[i-1] == ' ':
-              words = replaceCharacter(words, '', i) #check i-1 if its space there is a problem      
+# def checkPunctuation(words):
+#     #search each character for puntuation using a for loop in range length of string
+#     for i in range(len(words)):
+#       quoteCount = 0
+#           #check if its a punctuation character
+#       if words[i] == '"':
+#         quoteCount += 1
+#         break
+#       elif(words[i] in string.punctuation):
+#             if words[i-1] == ' ':
+             # words = replaceCharacter(words, '', i) #check i-1 if its space there is a problem      
 
-      if (words[i] == '"' and words[i]-1 == '.'):
+      # if (words[i] == '"' and words[i]-1 == '.'):
 
-        if (quoteCount%2 == 0):
-          words = replaceCharacter(words, ' "', i)
-        else:
-          words = replaceCharacter(words, '".', i-1)
+      #   if (quoteCount%2 == 0):
+      #     words = replaceCharacter(words, ' "', i)
+      #   else:
+      #     words = replaceCharacter(words, '".', i-1)
 
-      if(words[i] in string.punctuation):
+      # if(words[i] in string.punctuation):
 
-        if((words[i-1] == '.' and words[i+1] == '.') or (words[i-1] == '.' and words[i-2] == '.')) continue
+      #   if((words[i-1] == '.' and words[i+1] == '.') or (words[i-1] == '.' and words[i-2] == '.')) continue
         
-        if(words[i-1] in string.punctuation and words[i-1] != '"' and words[i-1] != ")"):
-          #TODO: Figure out punctuation which one takes priority and which one i remove
+      #   if(words[i-1] in string.punctuation and words[i-1] != '"' and words[i-1] != ")"):
+           #TODO: Figure out punctuation which one takes priority and which one i remove
 
       
-      if(quoteCount%2 == 1):
-        #TODO: Write code saying either too little or too many quotation marks
+      # if(quoteCount%2 == 1):
+         #TODO: Write code saying either too little or too many quotation marks
                       
+# def is_adj(word):
+#     part_of_speech = nltk.pos_tag(word)[0][1]
+#     return part_of_speech
 
 
-def main(data):
+def parseData(data):
+  # text - string
+  # array of paragraphs
+  # 
+  # return a text
+
   essay = json.load(data)
 
-  essayWordCount = {}
+  #essayWordCount = {}
 
-  essayAsString = ""
+  #essayAsString = ""  
 
-  for i in essay.keys("Sentences"):
+  essayAsArray = []
+
+  tool = language_tool_python.LanguageTool('en-CA')
+
+  for j in essays.keys("Paragraph"): 
+
+    sentenceChangedDict = {}
+    wikipediaArr = {}
+    sentenceChangedArray = []
     
-    sentenceWordCount = countWords(i)
+    for i in j.keys("Sentences"):
+      
+      newString = i
 
-    essayWordCount = mergeDictionary(essayWordCount, sentenceWordCount)
+      if(i == tool.correct(i)):
 
-    essayAsString = essayAsString + i
+        newString == tool.correct(i)
+
+      sentenceWordCount = countWords(newString)
+
+      for word,count in sentenceWordCount:
+
+        if (count >= 3):
+
+          newString = replaceWord(newString, word)
+
+      ## wikipedia link
+      ## voice
+
+      # [{
+      #   sentence: {info},
+      #   sentence: {info},
+      #   sentence: {info}
+      # },
+      # {
+      #   sentence: [Bool,String]
+      # }]
+      voice = app.get_voice(newString)
+
+      wikipediaArr = app.get_wikipedia_links(newString)
+
+      if (newString != i):
+
+        sentenceChangedDict[newString] = (True, voice)
+
+      else:
+        
+        sentenceChangedDict[newString] = (False, voice)
+
+      #essayWordCount = mergeDictionary(essayWordCount, sentenceWordCount)
+
+      sentenceChangedArray.append(sentenceChangedDict)
+
+
+    para = [sentenceChangedArray, wikipediaArr]
+    essayAsArray.append(para)    
+
+    #Array of Arrays of Array of Dictionaries
 
     #wait for api function call it here on each sentence
 
@@ -124,3 +186,5 @@ def replaceWord(sentence, wordToReplace):
       words[i] = dictionary.synonym(wordToReplace)
     
     newSentence = newSentence + " " + words[i]
+
+  return newSentence
